7d1c44f0172989a34f919076993911b6cee60bca
Tony Batts <tony.batts@knfilters.com>
2026-01-26 09:06:48 -0800

    configure images to not be optimized

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,14 +5,21 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    output: "export",
+    images: {
+      unoptimized: true
+    },
+    basePath: "/"
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
-    "next-env.d.ts",
-  ]),
+    "next-env.d.ts"
+  ])
 ]);
 
 export default eslintConfig;
